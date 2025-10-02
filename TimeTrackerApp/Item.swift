faad4551b7f6c39216cf45a5119d8bdbc8515fdc
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-02 09:46:38 +0200

    Initial Commit

---
+++ TimeTrackerApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TimeTrackerApp
+//
+//  Created by Azizbek Asadov on 02.10.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TimeTrackerAppTests/TimeTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerAppTests.swift
+//  TimeTrackerAppTests
+//
+//  Created by Azizbek Asadov on 02.10.2025.
+//
+
+import Testing
+@testable import TimeTrackerApp
+
+struct TimeTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
