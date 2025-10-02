98de70fa88fc0b993d920bdeda8d5785ef3f8025
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-02 09:53:18 +0200

    - TICKET: project setup
    - added project setup with raw planning of the project design
    - added configurations required for the project setup

---
+++ TimeTrackerApp/app/TimeTrackerAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  TimeTrackerAppApp.swift
+//  TimeTrackerApp
+//
+//  Created by Azizbek Asadov on 02.10.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TimeTrackerAppApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            VStack{}
+        }
+    }
+}
